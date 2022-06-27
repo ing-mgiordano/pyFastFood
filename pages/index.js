ac22aacac5ca90770590ffac1e6de437e69e7085
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-27 12:27:49 +0200

    se crea el layout

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../layout/Layout'
 
 export default function Home() {
+
   return (
-    <h1>next.js</h1>
+    
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
   )
 }
+
+
