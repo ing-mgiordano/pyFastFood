35ea4160a1c08ef6df4a9e7fc3d8e20552d1d459
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-27 12:45:03 +0200

    se crea componente sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+const Sidebar = () => {
+
+    return (
+        <>
+            <Image 
+                width={300}
+                height={100}
+                src='/img/logo.svg'
+                alt='imagen logotipo' 
+            />
+
+            
+        </>
+    )
+}
+
+export default Sidebar
